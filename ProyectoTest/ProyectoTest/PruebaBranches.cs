c713bfe24c45cb8c6cf76049df5ef951ad75b801
Alejandro Masso Martinez <almasso@ucm.es>
2022-03-21 12:41:56 +0100

    Trabajando con branches

---
+++ ProyectoTest/ProyectoTest/PruebaBranches.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ProyectoTest
+{
+    public class PruebaBranches
+    {
+        public PruebaBranches()
+        {
+            Console.WriteLine("Este archivo ha sido creado en una nueva branch");
+        }
+    }
+}
